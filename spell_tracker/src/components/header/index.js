a0b082a8131d394c052cce1267484a36ab0443ea
Shannon Kroll <skroll912@gmail.com>
2022-09-28 13:20:09 -0700

    header update

---
+++ spell_tracker/src/components/header/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+// import "~@flaticon/flaticon-uicons/css/all/all";
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+        <div className='header'>
+            <div className='header-logo'>
+                Spells List <img src='https://www.dnd5eapi.co/public/favicon.ico' alt='red 20 sided dice' style={{width: "40px"}}></img>
+            </div>
+            <div>
+                <a href='https://github.com/skroll13/reactProject'
+                target="blank"
+                rel="noopener noreferrer"
+                >
+
+            <button class="devicon-github-original"></button>
+          
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ spell_tracker/src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <div>topfold</div>
+        <div>SpellsList</div>
+    </div>
+  );
+};
+
+export default Home ;
