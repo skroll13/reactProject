c16ad2da43e5ad87d4721049e513bb08b8b31707
Shannon Kroll <skroll912@gmail.com>
2022-09-28 14:14:52 -0700

    pausing work for today

---
+++ spell_tracker/src/components/topfold/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './topfold.css'
+
+const TopFold = () => {
+  return (
+    <div className='topfold'>
+      <div className='home-topfold'>
+        <div className='search-bar'
+      </div>
+    </div>
+  )
+}
+
+export default TopFold
