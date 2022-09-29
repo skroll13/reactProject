8ecc71661f82f61f0dc2c2449bdfe46ca22fe48a
Shannon Kroll <skroll912@gmail.com>
2022-09-29 12:56:58 -0700

    style the search bar

--- spell_tracker/src/components/topfold/index.js
+++ spell_tracker/src/components/topfold/index.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './topfold.css'
 
 const TopFold = () => {
+  const [query, setQuery]= useState("")
+  const handleQuery=(e)=>{
+    setQuery(e.target.value)
+  }
   return (
     <div className='topfold'>
-      <div className='home-topfold'>
-        <div className='search-bar'
-      </div>
+      {window.location.pathname !== '/' ? <div className='home-topfold'>
+        <div className='search-bar'>
+          {/* the search icon isn't showing up. work on this if there's time  */}
+          {/* <a href="https://www.vecteezy.com/vector-art/4846677-magnifying-glass-icon-vector-search-loupe-lens-glass-magnifier-find-zoom-sign-icon-free-vector"></a> */}
+          <input
+          value={query}
+          placeholder='Search for Spells' 
+          onChange={(e)=>handleQuery(e)} 
+          />
+        </div>
+        <div className='add-button'>
+          <label>Add</label>
+
+        </div>
+      </div>:<div>Other topfold</div>}
+      
     </div>
   )
 }
