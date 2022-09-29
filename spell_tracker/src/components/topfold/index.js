b53872df18cc2321089c6c9dae6c85c360762c1d
Shannon Kroll <skroll912@gmail.com>
2022-09-29 14:33:14 -0700

    topfold styling complete

--- spell_tracker/src/components/topfold/index.js
+++ spell_tracker/src/components/topfold/index.js
@@ -8,7 +8,7 @@ const TopFold = () => {
   }
   return (
     <div className='topfold'>
-      {window.location.pathname !== '/' ? <div className='home-topfold'>
+      {window.location.pathname === '/' ? <div className='home-topfold'>
         <div className='search-bar'>
           {/* the search icon isn't showing up. work on this if there's time  */}
           {/* <a href="https://www.vecteezy.com/vector-art/4846677-magnifying-glass-icon-vector-search-loupe-lens-glass-magnifier-find-zoom-sign-icon-free-vector"></a> */}
@@ -22,7 +22,16 @@ const TopFold = () => {
           <label>Add</label>
 
         </div>
-      </div>:<div>Other topfold</div>}
+      </div>:(
+        <div className='add-topfold'>
+          <div className='add-topfold-button'>
+            <label>Back</label>
+          </div>
+          <div className='add-topfold-button'>
+            <label>Cancel</label>
+          </div>
+      </div>
+        )}
       
     </div>
   )
